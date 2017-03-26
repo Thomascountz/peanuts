97b9e8f5d681754f36a0f71bcd833c2930e86912
Thomas Countz <thomascountz@gmail.com>
2017-03-26 22:52:12 +0000

    updated static page routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
+  root 'static_pages#index'
 
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
