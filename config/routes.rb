73b2b9343c576261fb78ee38bac624481b778493
Thomas Countz <thomascountz@gmail.com>
2017-04-03 23:51:17 +0000

    added route for user#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  root 'static_pages#index'
-
+  resources :users, :only => [:show]
+  
   get '/about', to: 'static_pages#about'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'static_pages#index'
 end
