147ac1b407c783068dae1478728c004b4f92f4a4
Thomas Countz <thomascountz@gmail.com>
2017-04-03 16:05:44 +0000

    added user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'invalid user' do
+    it 'is invalid without a first name' do
+      user = build(:user, first_name: nil)
+      expect(user).to_not be_valid
+    end
+    it 'is invalid without a last name' do
+      user = build(:user, last_name: nil)
+      expect(user).to_not be_valid
+    end
+  end
+  
+  describe '#full_name' do
+    it 'returns first and last name' do
+      user = build(:user, first_name: "Joe", last_name: "Schmoe")
+      expect(user.full_name).to eq("Joe Schmoe")
+    end
+  end
 end
