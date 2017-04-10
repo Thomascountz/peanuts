3eea19b1d5b5129d90e8f06f8e624fcbaf24512b
Thomas Countz <thomascountz@gmail.com>
2017-04-10 15:02:17 +0200

    updated factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :event do
     title "This Is My Event"
     description "My even is so much fun, you all will enjoy it a lot, I promise!"
-    location "Lyceum Theatre"
+    location "149 W 45th St, New York, NY 10036, USA"
     manager
   end
 
