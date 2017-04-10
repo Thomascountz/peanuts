37dda756a486ff5ca7fcab0ed98208893cadbd57
Thomas Countz <thomascountz@gmail.com>
2017-04-10 18:35:03 +0200

    added event#new form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,8 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  def new
+    @event = Event.new
+  end
+
 end
