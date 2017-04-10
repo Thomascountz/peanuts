ef41f8159103cd6b4879f7a56cd65f6ef6b29553
Thomas Countz <thomascountz@gmail.com>
2017-04-10 18:36:42 +0200

    Added maxium event title length

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :manager, :class_name => "User"
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { minimum: 50, maximum: 450 }
   validates :location, presence: true
   validates :manager_id, presence: true
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Event, type: :model do
   describe 'validations' do
     before(:each) { event = build(:event) }
     it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_most(50)}
     it { should validate_presence_of(:description) }
     it { should validate_length_of(:description).is_at_least(50)}
     it { should validate_length_of(:description).is_at_most(450)}
