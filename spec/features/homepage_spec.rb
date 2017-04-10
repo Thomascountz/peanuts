e06b2ef429eff8e8769703504ea68b4653d68594
Thomas Countz <thomascountz@gmail.com>
2017-04-10 13:25:00 +0200

    updated tests

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -5,12 +5,12 @@ RSpec.feature "homepage", :type => :feature do
   before(:each) { visit "/" }
 
   context 'non-signed-in user' do
-  
+
     scenario 'User visits the hompage' do
       expect(page).to have_link('Log in', href: new_user_session_path)
-      expect(page).to have_link('sell tickets', href: '#')
-      expect(page).to have_link('buy tickets', href: '#')
+      expect(page).to have_link('sell tickets', href: coming_soon_path)
+      expect(page).to have_link('buy tickets', href: coming_soon_path)
     end
-    
+
   end
 end
