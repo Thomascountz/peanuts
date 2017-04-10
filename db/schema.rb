9d47e5c80157ef84332ba4cbbbac31e24f688f81
Thomas Countz <thomascountz@gmail.com>
2017-04-10 14:50:10 +0200

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403145928) do
+ActiveRecord::Schema.define(version: 20170410124709) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.text     "location"
+    t.integer  "manager_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
