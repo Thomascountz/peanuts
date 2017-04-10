1d60cb671cd2c108643b495dd299e284c122604f
Thomas Countz <thomascountz@gmail.com>
2017-04-10 13:12:54 +0200

    added coming soon route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   get '/dashboard', to: 'users#dashboard'
   get '/about', to: 'static_pages#about'
-  
+  get '/comingsoon', to: 'static_pages#coming_soon'
+
   root 'static_pages#index'
 end
