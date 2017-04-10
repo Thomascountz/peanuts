350815c89a9323e4dba19ea0994301cc5c5aa50e
Thomas Countz <thomascountz@gmail.com>
2017-04-10 18:14:43 +0200

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   get '/dashboard', to: 'users#dashboard'
   get '/about', to: 'static_pages#about'
   get '/coming_soon', to: 'static_pages#coming_soon'
+
+  resources :events
 
   root 'static_pages#index'
 end
