1961d53cec7cbd56b45ab1153aadb44c6377358d
Thomas Countz <thomascountz@gmail.com>
2017-04-10 15:53:58 +0200

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
-  validates :first_name, presence: true 
-  validates :last_name, presence: true 
-  
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  has_many :managed_events, :foreign_key => "manager_id", :class_name => "Event"
+
   def full_name
     "#{first_name} #{last_name}"
   end
