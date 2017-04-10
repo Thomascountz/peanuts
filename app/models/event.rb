d2ac21b46d03804024dfc6a65d27c3f123fcd761
Thomas Countz <thomascountz@gmail.com>
2017-04-10 20:42:22 +0200

    [ci skip] removed unneede validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :manager, :class_name => "User"
+  belongs_to :manager, :foreign_key => :manager_id, :class_name => "User"
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { minimum: 50, maximum: 450 }
   validates :location, presence: true
