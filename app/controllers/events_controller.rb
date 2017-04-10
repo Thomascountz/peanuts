a72a57a098ec2f78e61ddc92d63e84019c664fe4
Thomas Countz <thomascountz@gmail.com>
2017-04-10 20:39:36 +0200

    assigned foreign key to user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,23 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def new
-    @event = Event.new
+    @event = current_user.managed_events.build
   end
+
+  def create
+    @event = current_user.managed_events.build(event_params)
+    if @event.save
+      flash[:success] = "Event successfully created!"
+      redirect_to dashboard_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:title, :description, :location)
+    end
 
 end
