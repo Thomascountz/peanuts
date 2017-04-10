0c73c4a634966edde60f180f04cf2f48e7847a9f
Thomas Countz <thomascountz@gmail.com>
2017-04-10 13:24:54 +0200

    updated routes and hrefs

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/dashboard', to: 'users#dashboard'
   get '/about', to: 'static_pages#about'
-  get '/comingsoon', to: 'static_pages#coming_soon'
+  get '/coming_soon', to: 'static_pages#coming_soon'
 
   root 'static_pages#index'
 end
