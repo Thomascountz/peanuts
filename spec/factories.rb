ce9b188daa89da8cb71fdfe7a068ed86c34e7bf0
Thomas Countz <thomascountz@gmail.com>
2017-04-10 14:54:02 +0200

    updated factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,14 @@
 # Users
 FactoryGirl.define do
+
   factory :event do
-    title "MyString"
-    description "MyText"
-    location "MyText"
-    manager_id 1
+    title "This Is My Event"
+    description "My even is so much fun, you all will enjoy it a lot, I promise!"
+    location "Lyceum Theatre"
+    manager
   end
-  factory :user do
+
+  factory :user, aliases: [:manager] do
     first_name "John"
     last_name  "Doe"
     email { "#{first_name}#{last_name}@example.com" }
