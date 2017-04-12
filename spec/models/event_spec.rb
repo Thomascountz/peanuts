7c2e9b126fc7681581907e4fc75ce753a3935940
Thomas Countz <thomascountz@gmail.com>
2017-04-12 14:14:04 +0200

    added event validation and association specs

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  describe 'validations' do
+    before(:each) { event = build(:event) }
+    it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_most(50)}
+    it { should validate_presence_of(:description) }
+    it { should validate_length_of(:description).is_at_least(50)}
+    it { should validate_length_of(:description).is_at_most(450)}
+    it { should validate_presence_of(:location) }
+    it { should belong_to(:manager).class_name('User') }
+  end
+end
