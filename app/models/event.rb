359404563c11a20dce8514cc107f2887197bcb7f
Thomas Countz <thomascountz@gmail.com>
2017-04-12 14:35:43 +0200

    greenlit event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :manager, :foreign_key => :manager_id, :class_name => "User"
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { minimum: 50, maximum: 450 }
+  validates :location, presence: true, length: { maximum: 250 }
 end
