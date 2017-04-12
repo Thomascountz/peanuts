13ab06f740ba703801d5c4669437d1522c0204f6
Thomas Countz <thomascountz@gmail.com>
2017-04-12 14:13:47 +0200

    added event factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,19 @@
 # Users
 FactoryGirl.define do
-  factory :user do
+
+  factory :user, aliases: [:manager] do
     first_name "John"
     last_name  "Doe"
     email { "#{first_name}#{last_name}@example.com" }
     password "password"
     password_confirmation "password"
   end
+
+  factory :event do
+    title "This is My Event"
+    description "My event is so much fun, you will all enjoy it alot! My Promise"
+    location "149 W 45th St, New York, NY 10036, USA"
+    manager
+  end
+
 end
