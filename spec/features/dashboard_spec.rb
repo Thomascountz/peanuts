be0d828a952bf7dfa902fb4140621539617cfdb7
Thomas Countz <thomascountz@gmail.com>
2017-04-12 15:17:31 +0200

    added specs for event creation

---
+++ spec/features/dashboard_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.feature "event", :type => :feature do
+
+  context 'When a user is not signed in' do
+    scenario 'They try to visit their dashboard' do
+      visit '/dashboard'
+      expect(current_path).to eq('/users/sign_in')
+      expect('.alert').to be_present
+    end
+  end
+
+  context 'when user is signed in' do
+    scenario 'User creates a new event with valid inputs' do
+      user = create(:user)
+      event = build_stubbed(:event)
+      login_as(user)
+      visit '/dashboard'
+      page.click_button('sell tickets')
+      expect(current_path).to eq('/events/new')
+      page.fill_in('Title', with: event.title)
+      page.fill_in('Description', with: event.description)
+      page.fill_in('Location', with: event.location)
+      page.click_button('Submit')
+      expect(current_path).to eq('/dashboard')
+      expect('.alert').to be_present
+      expect(body).to have_content(event.title)
+    end
+
+    xscenario 'User creates a new event with invalid inputs' do
+      user = create(:user)
+      event = build_stubbed(:event, title: "",
+                                    description: "this is too short.",
+                                    location: "")
+      login_as(user)
+      visit '/events/new'
+      page.fill_in('Title', with: event.title)
+      page.fill_in('Description', with: event.description)
+      page.fill_in('Location', with: event.location)
+      page.click_button('Create event')
+      expect(current_path).to_not eq('/dashboard')
+      expect('.error_explainations'). to be_present
+    end
+  end
+end
