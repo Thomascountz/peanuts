bb8084b535c5bce37e4b2f635b295cb072dcaec8
Thomas Countz <thomascountz@gmail.com>
2017-04-12 20:59:25 +0200

    created db seed for demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Demo user
+user = User.create( first_name:                 "Dan",
+                    last_name:                 "Demo",
+                    email:      "demodan@example.com",
+                    password:              "password",
+                    password_confirmation: "password" )
+
+user.managed_events.create( title:       "Dan's Secret Garden",
+                            description: "This is your change to see me in action. You will never get to experience anything like this again, so do yourself a favor and book now.",
+                            location:    "Dixon Place Theater" )
+
+user.managed_events.create( title:       "Dan's Secret Garden II",
+                            description: "Oh man, I'm back! In case you missed it last time, here I am again in full glory! Come and witness a show like you've never witnessed before",
+                            location:    "Byham Theatre Pittsburgh" )
+
+user.managed_events.create( title:       "Piggly Wiggly",
+                            description: "Oink Oink? More like Shaboink Shaboink! That's right, Piggly Wiggly will have you bouncing in your seats! Come see my premiere DJ set this weekend!",
+                            location:    "Signature Theater" )
