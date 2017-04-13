e10b17ab23779fc6dc6286d471d09d3da157c7f5
Thomas Countz <thomascountz@gmail.com>
2017-04-13 16:32:39 +0200

    fixed typo in scope definition'

--- app/models/event_time.rb
+++ app/models/event_time.rb
@@ -2,7 +2,7 @@ class EventTime < ApplicationRecord
   validates :start_time, presence: true
   validates :end_time, presence: true
   belongs_to :event
-  default_scope -> { order(start_time: :desc) }
+  default_scope -> { order(start_time: :asc) }
   scope :upcoming, -> { where('start_time >= ?', Time.zone.now) }
-  scope :past, -> { where ('start_time <= ?', Time.zone.now) }
+  scope :past, -> { where('start_time <= ?', Time.zone.now) }
 end
