ea68777fd2304dc88eea3be95ac7f84dc9947534
Thomas Countz <thomascountz@gmail.com>
2017-04-13 14:53:52 +0200

    added model specs for event_time

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,6 @@
 # Users
 FactoryGirl.define do
-  factory :event_time do
-    start_time "2017-04-13 14:41:27"
-    end_time "2017-04-13 14:41:27"
-    event_id 1
-  end
-
+  
   factory :user, aliases: [:manager] do
     first_name "John"
     last_name  "Doe"
--- spec/models/event_time_spec.rb
+++ spec/models/event_time_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe EventTime, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) { event_time = build(:event_time) }
+  it { should validate_presence_of(:start_time) }
+  it { should validate_presence_of(:end_time) }
+  it { should belong_to(:manager).class_name('User') }
 end
