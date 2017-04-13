8f3b9aac301adffce862f97c5450236dcb79d8cb
Thomas Countz <thomascountz@gmail.com>
2017-04-13 11:24:21 +0200

    updated user specs to use shoulda matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-  describe 'invalid user' do
-    it 'is invalid without a first name' do
-      user = build(:user, first_name: nil)
-      expect(user).to_not be_valid
-    end
-    it 'is invalid without a last name' do
-      user = build(:user, last_name: nil)
-      expect(user).to_not be_valid
-    end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
   end
-  
+
   describe '#full_name' do
     it 'returns first and last name' do
       user = build(:user, first_name: "Joe", last_name: "Schmoe")
