88166328f634187f42cf51ba915d66e51f17262b
Thomas Countz <thomascountz@gmail.com>
2017-04-13 16:00:47 +0200

    aded scopes

--- app/models/event_time.rb
+++ app/models/event_time.rb
@@ -2,4 +2,7 @@ class EventTime < ApplicationRecord
   validates :start_time, presence: true
   validates :end_time, presence: true
   belongs_to :event
+  default_scope -> { order(start_time: :desc) }
+  scope :upcoming, -> { where('start_time >= ?', Time.zone.now) }
+  scope :past, -> { where ('start_time <= ?', Time.zone.now) }
 end
