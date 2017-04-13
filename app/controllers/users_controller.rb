30e2e7b8e037d6ceebf036725b878572f456ccdb
Thomas Countz <thomascountz@gmail.com>
2017-04-13 10:36:37 +0200

    added demo sign in method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:dashboard]
-  
+
   def dashboard
     @user = current_user
+  end
+
+  # Signs in as demo user
+  def demo_sign_in
+  user = User.find_by(email: 'demodan@example.com')
+  sign_in(user)
+  redirect_to root_url
   end
 end
