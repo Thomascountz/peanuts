17c45f8446e5a1b1d32ff520eed90b2aae39c12a
Thomas Countz <thomascountz@gmail.com>
2017-04-14 10:58:56 +0200

    [WIP][ci skip] attempt to validate end time is after start time

--- app/models/event_time.rb
+++ app/models/event_time.rb
@@ -1,6 +1,8 @@
 class EventTime < ApplicationRecord
   validates :start_time, presence: true
   validates :end_time, presence: true
+  # NOT WORKING
+  validate :start_time_before_end_time
   belongs_to :event
   default_scope -> { order(start_time: :asc) }
   scope :upcoming, -> { where('start_time >= ?', Time.zone.now) }
@@ -9,5 +11,14 @@ class EventTime < ApplicationRecord
   # returns true if the event hasn't begun yet
   def upcoming?
     start_time >= Time.zone.now
+  end
+
+  private
+
+  # NOT WORKING
+  def start_time_before_end_time
+    if self.start_time >= self.end_time
+      errors.add(:end_time, "must be after start time")
+    end
   end
 end
--- spec/models/event_time_spec.rb
+++ spec/models/event_time_spec.rb
@@ -7,6 +7,22 @@ RSpec.describe EventTime, type: :model do
     it { should validate_presence_of(:end_time) }
     it { should belong_to(:event) }
   end
+
+  # FAILS
+  describe 'end time validation' do
+
+    it 'should be valid' do
+      event_time = build(:event_time)
+      expect(event_time).to be_valid
+    end
+
+    it 'should be invalid if start time is after end time' do
+      event_time = build(:event_time, start_time: Time.zone.now + 2.hours,
+                                      end_time:   Time.zone.now + 1.hours)
+      expect(event_time).to be_invalid
+    end
+
+  end
   describe '#upcoming?' do
     it 'returns true if the event is starting after now' do
       event_time = build_stubbed(:event_time, start_time: Time.zone.now + 1.hour)
