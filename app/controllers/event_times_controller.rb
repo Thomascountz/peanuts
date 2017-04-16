dfb06a2744cadae510c0523974627f1a7a5af7c9
Thomas Countz <thomascountz@gmail.com>
2017-04-16 22:27:52 +0200

    added event times new form

--- app/controllers/event_times_controller.rb
+++ app/controllers/event_times_controller.rb
@@ -1,2 +1,12 @@
 class EventTimesController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
+  def new
+    @event = Event.find(params[:event_id])
+    @event_time = @event.event_times.build
+  end
+
+  def create
+
+  end
 end
