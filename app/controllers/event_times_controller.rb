b9e8643efd000460d7d2dd8c0739aff04ad5fdf6
Thomas Countz <thomascountz@gmail.com>
2017-04-16 22:33:26 +0200

    added create action

--- app/controllers/event_times_controller.rb
+++ app/controllers/event_times_controller.rb
@@ -1,5 +1,5 @@
 class EventTimesController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def new
     @event = Event.find(params[:event_id])
@@ -7,6 +7,19 @@ class EventTimesController < ApplicationController
   end
 
   def create
-
+    @event = Event.find(params[:event_id])
+    @event_time = @event.event_times.build(event_time_params)
+    if @event_time.save
+      flash[:success] = "Your event was created!"
+      redirect_to event_path(@event)
+    else
+      render 'new'
+    end
   end
+
+  private
+
+    def event_time_params
+      params.require(:event_time).permit(:start_time, :end_time)
+    end
 end
