73a02fe9aea7a1ea68b9905f6e61a6da0e02640d
Thomas Countz <thomascountz@gmail.com>
2017-04-17 19:51:21 +0200

    added authentication before edit and update event times

--- app/controllers/event_times_controller.rb
+++ app/controllers/event_times_controller.rb
@@ -1,5 +1,5 @@
 class EventTimesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def new
     @event = Event.find(params[:event_id])
