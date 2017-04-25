64c5bcb52cfaefa3829c4511b964abe611952efe
Thomas Countz <thomascountz@gmail.com>
2017-04-25 11:50:04 -0400

    removed unneeded method

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -75,16 +75,4 @@ RSpec.feature "event", :type => :feature do
       expect(body).to_not have_content(event.title)
     end
   end
-
-  private
-
-    # fills in appropirate datetime select boxes with given datetime object
-    def select_date_and_time(date, options = {})
-      field = options[:from]
-      select date.strftime('%Y'),    :from => "#{field}_1i" #year
-      select date.strftime('%B'),    :from => "#{field}_2i" #month
-      select date.strftime('%d'),    :from => "#{field}_3i" #day
-      select date.strftime('%I %p'), :from => "#{field}_4i" #hour
-      select date.strftime('%M'),    :from => "#{field}_5i" #minute
-    end
 end
