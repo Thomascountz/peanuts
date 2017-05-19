5d210919bb14d0734cf3275cd5de60f07e10ad01
Thomas Countz <thomascountz@gmail.com>
2017-05-19 17:18:35 -0400

    added test for empty database

--- spec/features/event_index_spec.rb
+++ spec/features/event_index_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "event index page", :type => :feature do
 
 	let(:event1) { create(:event) }
 
-	context 'when vising the site' do
+	context 'when events exist in the database' do
 
 		before { event1 }
 
@@ -15,6 +15,13 @@ RSpec.feature "event index page", :type => :feature do
 	 		expect(page).to have_content(event1.location)
 	 		expect(page).to have_link('more info', href: event_path(event1))
 	 	end
+	end
+
+	context 'when there are no events in the database' do
+		scenario 'user visits the event index page' do
+			visit events_path
+			expect(page).to have_link('', href: new_event_path)
+		end
 	end
 
 end
