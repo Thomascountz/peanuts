16709934463cd8702b93a02fe835d9af44749651
Thomas Countz <thomascountz@gmail.com>
2017-05-19 14:31:28 -0400

    fixes #75 remove user id from event show page

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,7 +2,7 @@ class TicketsController < ApplicationController
 
   def create
   	event_time = EventTime.find(params[:ticket][:event_time_id])
-  	attendee = User.find(params[:ticket][:attendee_id])
+  	attendee = current_user
   	ticket = Ticket.new(event_time: event_time, attendee: attendee)
   	if ticket.save
   		flash[:success] = "You've purchsed a ticket for #{event_time.event.title}!"
